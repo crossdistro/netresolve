cc951a292717ebc759c4ac36540b297e9b01181d
Pavel Šimerda <psimerda@redhat.com>
2014-11-10 22:09:33 +0100

    dns: refactor thoroughly, add tests
    Tests aren't being run as part of 'make check' as they rely on
    working DNS configuration.

--- backends/dns.c
+++ backends/dns.c
@@ -22,8 +22,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <netresolve-backend.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <ldns/ldns.h>
 
 #if defined(USE_UNBOUND)
@@ -49,19 +47,63 @@ struct priv_dns {
 	const uint8_t *address;
 	int cls;
 	int type;
-	ldns_pkt *ip4_pkt;
-	ldns_pkt *ip6_pkt;
+	int pending;
+	bool answered;
+	bool failed;
 #if defined(USE_UNBOUND)
 	struct ub_ctx* ctx;
 #elif defined(USE_ARES)
 	ares_channel channel;
-	fd_set rfds;
-	fd_set wfds;
+	fd_set rfds, wfds;
 	int nfds;
 #endif
 };
 
-#if defined(USE_ARES)
+static void apply_answer(struct priv_dns *priv, uint8_t *answer, size_t length);
+
+#if defined(USE_UNBOUND)
+
+static void
+callback(void *arg, int status, struct ub_result* result)
+{
+	struct priv_dns *priv = arg;
+
+	priv->pending--;
+
+	if (status) {
+		error("libunbound: %s", ub_strerror(status));
+		priv->failed = true;
+		return;
+	}
+
+	apply_answer(priv, result->answer_packet, result->answer_len);
+
+	ub_resolve_free(result);
+}
+
+#elif defined(USE_ARES)
+
+static void
+callback(void *arg, int status, int timeouts, unsigned char *abuf, int alen)
+{
+	struct priv_dns *priv = arg;
+
+	priv->pending--;
+
+	switch (status) {
+	case ARES_EDESTRUCTION:
+		abort();
+	case ARES_SUCCESS:
+	case ARES_ENOTFOUND:
+		break;
+	default:
+		error("ares: %s", ares_strerror(status));
+		priv->failed = true;
+		return;
+	}
+
+	apply_answer(priv, abuf, alen);
+}
 
 static void
 watch_file_descriptors(struct priv_dns *priv)
@@ -76,10 +118,6 @@ watch_file_descriptors(struct priv_dns *priv)
 		if (FD_ISSET(fd, &priv->wfds))
 			netresolve_backend_watch_fd(priv->query, fd, POLLOUT);
 	}
-
-	/* FIXME: In some cases this might rather be failed. */
-	if (!priv->nfds)
-		netresolve_backend_finished(priv->query);
 }
 
 static void
@@ -90,6 +128,7 @@ unwatch_file_descriptors(struct priv_dns *priv)
 	for (int fd = 0; fd < priv->nfds; fd++)
 		if (FD_ISSET(fd, &priv->rfds))
 			netresolve_backend_unwatch_fd(priv->query, fd);
+
 	FD_ZERO(&priv->rfds);
 	FD_ZERO(&priv->wfds);
 	priv->nfds = 0;
@@ -97,96 +136,18 @@ unwatch_file_descriptors(struct priv_dns *priv)
 
 #endif
 
-static void lookup_host(struct priv_dns *priv);
-
 static void
-#if defined(USE_UNBOUND)
-callback(void *arg, int status, struct ub_result* result)
-#elif defined(USE_ARES)
-callback(void *arg, int status, int timeouts, unsigned char *abuf, int alen)
-#endif
+lookup(struct priv_dns *priv, const char *name, int type, int class)
 {
-	struct priv_dns *priv = arg;
-	netresolve_query_t query = priv->query;
-	const uint8_t *answer;
-	size_t length;
+	debug("looking up %s record for %s", ldns_rr_descript(type)->_name, name);
+
+	priv->pending++;
 
 #if defined(USE_UNBOUND)
-	if (status) {
-		error("libunbound: %s", ub_strerror(status));
-		return;
-	}
-
-	answer = result->answer_packet;
-	length = result->answer_len;
+	ub_resolve_async(priv->ctx, name, type, class, priv, callback, NULL);
 #elif defined(USE_ARES)
-	switch (status) {
-	case ARES_EDESTRUCTION:
-		return;
-	case ARES_SUCCESS:
-		break;
-	default:
-		error("ares: %s", ares_strerror(status));
-		return;
-	}
-
-	answer = abuf;
-	length = alen;
+	ares_query(priv->channel, name, class, type, callback, priv);
 #endif
-
-	if (priv->type) {
-		netresolve_backend_set_dns_answer(query, answer, length);
-		netresolve_backend_finished(query);
-		return;
-	}
-
-	ldns_pkt *pkt;
-
-	if (ldns_wire2pkt(&pkt, answer, length)) {
-		error("can't parse the DNS answer");
-		netresolve_backend_failed(query);
-		return;
-	}
-
-#if defined(USE_UNBOUND)
-	ub_resolve_free(result);
-#endif
-
-	debug("received %s record", ldns_rr_descript(pkt->_question->_rrs[0]->_rr_type)->_name);
-
-	switch (pkt->_question->_rrs[0]->_rr_type) {
-	case LDNS_RR_TYPE_A:
-		priv->ip4_pkt = pkt;
-		return;
-	case LDNS_RR_TYPE_AAAA:
-		priv->ip6_pkt = pkt;
-		return;
-	case LDNS_RR_TYPE_SRV:
-		/* FIXME: We only support one SRV record per name. */
-		free(priv->name);
-		priv->priority = ldns_rdf2native_int16(pkt->_answer->_rrs[0]->_rdata_fields[0]);
-		priv->weight = ldns_rdf2native_int16(pkt->_answer->_rrs[0]->_rdata_fields[1]);
-		priv->port = ldns_rdf2native_int16(pkt->_answer->_rrs[0]->_rdata_fields[2]);
-		priv->name = ldns_rdf2str(pkt->_answer->_rrs[0]->_rdata_fields[3]);
-		lookup_host(priv);
-		break;
-	case LDNS_RR_TYPE_PTR:
-		/* FIXME: We only support one PTR record. */
-		if (!pkt->_answer->_rr_count) {
-			netresolve_backend_failed(query);
-			break;
-		}
-		char *name = ldns_rdf2str(pkt->_answer->_rrs[0]->_rdata_fields[0]);
-		netresolve_backend_add_name_info(query, name, NULL);
-		free(name);
-		netresolve_backend_finished(query);
-		break;
-	default:
-		error("ignoring unknown response");
-		netresolve_backend_failed(query);
-	}
-
-	ldns_pkt_free(pkt);
 }
 
 static const char *
@@ -205,35 +166,20 @@ protocol_to_string(int proto)
 }
 
 static void
-lookup(struct priv_dns *priv, const char *name, int type, int class)
-{
-	debug("looking up %s record for %s", ldns_rr_descript(type)->_name, priv->name);
-#if defined(USE_UNBOUND)
-	ub_resolve_async(priv->ctx, name, type, class, priv, callback, NULL);
-#elif defined(USE_ARES)
-	ares_query(priv->channel, name, class, type, callback, priv);
-#endif
-}
-
-static void
 lookup_srv(struct priv_dns *priv)
 {
 	char *name;
 
-	priv->protocol = netresolve_backend_get_protocol(priv->query);
-
 	if (asprintf(&name, "_%s._%s.%s",
 			netresolve_backend_get_servname(priv->query),
 			protocol_to_string(priv->protocol),
-			netresolve_backend_get_nodename(priv->query)) == -1) {
+			netresolve_backend_get_nodename(priv->query)) != -1) {
+		lookup(priv, name, LDNS_RR_TYPE_SRV, LDNS_RR_CLASS_IN);
+		free(name);
+	} else {
 		error("memory allocation failed");
-		netresolve_backend_failed(priv->query);
-		return;
+		priv->failed = true;
 	}
-
-	lookup(priv, name, LDNS_RR_TYPE_SRV, LDNS_RR_CLASS_IN);
-
-	free(name);
 }
 
 static void
@@ -279,9 +225,7 @@ lookup_address(struct priv_dns *priv)
 				priv->address[0x0] & 0x0f, priv->address[0x0] >> 4);
 		break;
 	default:
-		error("unknown address family: %d", priv->family);
-		netresolve_backend_failed(priv->query);
-		return;
+		abort();
 	}
 
 	lookup(priv, name, LDNS_RR_TYPE_PTR, LDNS_RR_CLASS_IN);
@@ -293,42 +237,25 @@ lookup_dns(struct priv_dns *priv)
 	lookup(priv, priv->name, priv->type, priv->cls);
 }
 
-
-static bool
-apply_pkt(netresolve_query_t query, const ldns_pkt *pkt)
+static void
+apply_addresses(struct priv_dns *priv, const ldns_rr_list *answer)
 {
-	if (!pkt)
-		return true;
-
-	struct priv_dns *priv = netresolve_backend_get_priv(query);
-	int rcode = ldns_pkt_get_rcode(pkt);
-
-	if (rcode) {
-		error("rcode: %d", rcode);
-		return false;
-	}
-
-	ldns_rr_list *answer = ldns_pkt_answer(pkt);
-
 	for (int i = 0; i < answer->_rr_count; i++) {
 		ldns_rr *rr = answer->_rrs[i];
 
 		switch (rr->_rr_type) {
 		case LDNS_RR_TYPE_CNAME:
-			{
-				char *name = ldns_rdf2str(rr->_rdata_fields[0]);
-				netresolve_backend_set_canonical_name(query, name);
-				free(name);
-			}
+			free(priv->name);
+			priv->name = ldns_rdf2str(rr->_rdata_fields[0]);
 			break;
 		case LDNS_RR_TYPE_A:
-			netresolve_backend_add_path(query,
+			netresolve_backend_add_path(priv->query,
 					AF_INET, rr->_rdata_fields[0]->_data, 0,
 					0, priv->protocol, priv->port,
 					priv->priority, priv->weight, rr->_ttl);
 			break;
 		case LDNS_RR_TYPE_AAAA:
-			netresolve_backend_add_path(query,
+			netresolve_backend_add_path(priv->query,
 					AF_INET6, rr->_rdata_fields[0]->_data, 0,
 					0, priv->protocol, priv->port,
 					priv->priority, priv->weight, rr->_ttl);
@@ -337,18 +264,96 @@ apply_pkt(netresolve_query_t query, const ldns_pkt *pkt)
 			break;
 		}
 	}
-
-	return true;
 }
 
-struct priv_dns *
+static void
+apply_answer(struct priv_dns *priv, uint8_t *data, size_t length)
+{
+	assert(data);
+	assert(length);
+
+	if (priv->type) {
+		netresolve_backend_set_dns_answer(priv->query, data, length);
+		priv->answered = true;
+		return;
+	}
+
+	ldns_pkt *pkt;
+
+	if (ldns_wire2pkt(&pkt, data, length)) {
+		error("can't parse the DNS answer");
+		priv->failed = true;
+		return;
+	}
+
+	ldns_pkt_rcode rcode = ldns_pkt_get_rcode(pkt);
+	ldns_rr_type type = ldns_pkt_question(pkt)->_rrs[0]->_rr_type;
+	ldns_rr_list *answer = ldns_pkt_answer(pkt);
+
+	switch (rcode) {
+	case 0:
+		break;
+	case LDNS_RCODE_NXDOMAIN:
+		debug("%s record not found (%d queries left)",
+				ldns_rr_descript(type)->_name,
+				priv->pending);
+		if (type == LDNS_RR_TYPE_SRV)
+			lookup_host(priv);
+		else
+			priv->failed = true;
+		goto out;
+	default:
+		error("rcode: %d", rcode);
+		priv->failed = true;
+		goto out;
+	}
+
+	debug("%s record found (%d queries left)",
+			ldns_rr_descript(type)->_name,
+			priv->pending);
+
+	switch (type) {
+	case LDNS_RR_TYPE_A:
+	case LDNS_RR_TYPE_AAAA:
+		apply_addresses(priv, answer);
+		priv->answered = true;
+		break;
+	case LDNS_RR_TYPE_SRV:
+		/* FIXME: We only support one SRV record per name. */
+		priv->priority = ldns_rdf2native_int16(answer->_rrs[0]->_rdata_fields[0]);
+		priv->weight = ldns_rdf2native_int16(answer->_rrs[0]->_rdata_fields[1]);
+		priv->port = ldns_rdf2native_int16(answer->_rrs[0]->_rdata_fields[2]);
+
+		free(priv->name);
+		priv->name = ldns_rdf2str(answer->_rrs[0]->_rdata_fields[3]);
+		lookup_host(priv);
+		break;
+	case LDNS_RR_TYPE_PTR:
+		/* FIXME: We only support one PTR record. */
+		if (!answer->_rr_count) {
+			netresolve_backend_failed(priv->query);
+			break;
+		}
+		free(priv->name);
+		priv->name = ldns_rdf2str(answer->_rrs[0]->_rdata_fields[0]);
+		priv->answered = true;
+		break;
+	default:
+		abort();
+	}
+
+out:
+	ldns_pkt_free(pkt);
+}
+
+static struct priv_dns *
 setup(netresolve_query_t query, char **settings)
 {
 	struct priv_dns *priv = netresolve_backend_new_priv(query, sizeof *priv);
 	int status;
 
 	if (!priv)
-		goto fail;
+		return NULL;;
 
 	const char *name = netresolve_backend_get_nodename(query);
 	priv->name = name ? strdup(name) : NULL;
@@ -359,15 +364,13 @@ setup(netresolve_query_t query, char **settings)
 #if defined(USE_UNBOUND)
 	priv->ctx = ub_ctx_create();
 	if(!priv->ctx)
-		goto fail;
+		return NULL;;
 
 	if ((status = ub_ctx_resolvconf(priv->ctx, NULL)) != 0) {
 		error("libunbound: %s", ub_strerror(status));
-		goto fail;
+		return NULL;;
 	}
 	netresolve_backend_watch_fd(query, ub_fd(priv->ctx), POLLIN);
-
-	return priv;
 #elif defined(USE_ARES)
 	/* ares doesn't seem to accept const options */
 	static struct ares_options options = {
@@ -376,21 +379,19 @@ setup(netresolve_query_t query, char **settings)
 	};
 
 	status = ares_library_init(ARES_LIB_INIT_ALL);
-	if (status != ARES_SUCCESS)
-		goto fail_ares;
+	if (status != ARES_SUCCESS) {
+		error("ares library: %s", ares_strerror(status));
+		return NULL;
+	}
 	status = ares_init_options(&priv->channel, &options,
 			ARES_OPT_FLAGS | ARES_OPT_LOOKUPS);
-	if (status != ARES_SUCCESS)
-		goto fail_ares;
-
-	return priv;
-fail_ares:
-	error("ares: %s", ares_strerror(status));
+	if (status != ARES_SUCCESS) {
+		error("ares channel: %s", ares_strerror(status));
+		return NULL;
+	}
 #endif
 
-fail:
-	netresolve_backend_failed(query);
-	return NULL;
+	return priv;
 }
 
 void
@@ -398,17 +399,15 @@ setup_forward(netresolve_query_t query, char **settings)
 {
 	struct priv_dns *priv;
 
-	if (!(priv = setup(query, settings)))
-		return;
-
-	if (!priv->name) {
+	if (!(priv = setup(query, settings)) || !priv->name) {
 		netresolve_backend_failed(query);
 		return;
 	}
 
-	if (netresolve_backend_get_dns_srv_lookup(query))
+	if (netresolve_backend_get_dns_srv_lookup(query)) {
+		priv->protocol = netresolve_backend_get_protocol(priv->query);
 		lookup_srv(priv);
-	else
+	} else
 		lookup_host(priv);
 
 #if defined(USE_ARES)
@@ -421,10 +420,7 @@ setup_reverse(netresolve_query_t query, char **settings)
 {
 	struct priv_dns *priv;
 
-	if (!(priv = setup(query, settings)))
-		return;
-
-	if (!(priv->address = netresolve_backend_get_address(query))) {
+	if (!(priv = setup(query, settings)) || !(priv->address = netresolve_backend_get_address(query))) {
 		netresolve_backend_failed(query);
 		return;
 	}
@@ -441,10 +437,7 @@ setup_dns(netresolve_query_t query, char **settings)
 {
 	struct priv_dns *priv;
 
-	if (!(priv = setup(query, settings)))
-		return;
-
-	if (!priv->name) {
+	if (!(priv = setup(query, settings)) || !priv->name) {
 		netresolve_backend_failed(query);
 		return;
 	}
@@ -465,32 +458,28 @@ dispatch(netresolve_query_t query, int fd, int events)
 #if defined(USE_UNBOUND)
 	ub_process(priv->ctx);
 #elif defined(USE_ARES)
-	int rfd = events & POLLIN ? fd : ARES_SOCKET_BAD;
-	int wfd = events & POLLOUT ? fd : ARES_SOCKET_BAD;
-
 	unwatch_file_descriptors(priv);
-	ares_process_fd(priv->channel, rfd, wfd);
-#endif
-
-	bool ip4_done = !!priv->ip4_pkt;
-	bool ip6_done = !!priv->ip6_pkt;
-
-	if (priv->family == AF_INET)
-		ip6_done = true;
-	if (priv->family == AF_INET6)
-		ip4_done = true;
-
-	if (ip4_done && ip6_done) {
-		if (apply_pkt(query, priv->ip4_pkt) && apply_pkt(query, priv->ip6_pkt))
-			netresolve_backend_finished(query);
-		else
-			netresolve_backend_failed(query);
-		return;
-	}
-
-#if defined(USE_ARES)
+	ares_process_fd(priv->channel,
+			events & POLLIN ? fd : ARES_SOCKET_BAD,
+			events & POLLOUT ? fd : ARES_SOCKET_BAD);
 	watch_file_descriptors(priv);
 #endif
+
+	if (!priv->pending) {
+		if (priv->answered && priv->name) {
+			char *last = priv->name + strlen(priv->name) - 1;
+
+			if (*last == '.')
+				*last = '\0';
+
+			netresolve_backend_set_canonical_name(query, priv->name);
+		}
+
+		if (priv->failed)
+			netresolve_backend_failed(query);
+		else
+			netresolve_backend_finished(query);
+	}
 }
 
 void
@@ -498,12 +487,7 @@ cleanup(netresolve_query_t query)
 {
 	struct priv_dns *priv = netresolve_backend_get_priv(query);
 
-	if (priv->ip4_pkt)
-		ldns_pkt_free(priv->ip4_pkt);
-	if (priv->ip6_pkt)
-		ldns_pkt_free(priv->ip6_pkt);
-	if (priv->name)
-		free(priv->name);
+	free(priv->name);
 
 #if defined(USE_UNBOUND)
 	if (priv->ctx) {
