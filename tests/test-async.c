cac0eb7f58440811f8c3b2a997ec92a80c0d26e3
Pavel Šimerda <psimerda@redhat.com>
2015-10-19 11:03:54 +0200

    fix up copyright notices

--- tests/test-async.c
+++ tests/test-async.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013 Pavel Šimerda, Red Hat, Inc. (psimerda at redhat.com) and others
+/* Copyright (c) 2013+ Pavel Šimerda, Red Hat, Inc. (psimerda at redhat.com) and others
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
